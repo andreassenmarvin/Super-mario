0bbd5ce70d5fd910ba8c5d1f59afaa299a07ecd8
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-05 13:25:56 +0300

    Create biggify function

--- app.js
+++ app.js
@@ -104,3 +104,34 @@ scene("game", ({ level, score }) => {
   ])
 
   add([text('level ' + parseInt(level + 1) ), pos(40, 6)])
+
+  function big() {
+    let timer = 0
+    let isBig = false
+    return {
+      update() {
+        if (isBig) {
+          CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+          timer -= dt()
+          if (timer <= 0) {
+            this.smallify()
+          }
+        }
+      },
+      isBig() {
+        return isBig
+      },
+      smallify() {
+        this.scale = vec2(1)
+        CURRENT_JUMP_FORCE = JUMP_FORCE
+        timer = 0
+        isBig = false
+      },
+      biggify(time) {
+        this.scale = vec2(2)
+        timer = time
+        isBig = true     
+      }
+    }
+  }
+
